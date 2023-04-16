9cd2c88f4b3d9433b017f32528a5b037b0417b7c
Conner Aldrich <connercaldrich@gmail.com>
2023-04-16 11:14:24 -0500

    Style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,8 +26,8 @@
         <h1 class="text-3xl font-bold underline">Stable Diffusion!</h1>
         <p>Example made for <i><b>Intelligent Svelte</b></i>.</p>
     </div>
-    
-    <form method="POST" action="/api/prompt" use:enhance={api_prompt} class="flex flex-col space-y-2 md:min-w-[28rem] lg:min-w-[32rem] xl:min-w-[36rem] max-w-6xl">
+
+    <form method="POST" action="/api/prompt" use:enhance={api_prompt} class="image-form">
         {#if loading}
             <div class="flex flex-col items-center justify-center w-full aspect-square border-2 border-neutral-300 border-dashed rounded-lg cursor-pointer bg-neutral-50 text-transparent">
                 <svg class="my-auto animate-spin h-6 w-6 text-black" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -36,20 +36,12 @@
                 </svg>
             </div>
         {:else}
-            <img alt="Generated Output" src={image} class="flex flex-col items-center justify-center w-full aspect-square border-2 border-neutral-300 border-dashed rounded-lg cursor-pointer bg-neutral-50 text-transparent" />
+            <img alt="Generated Output" src={image} class="image-output" />
         {/if}
 
         <span class="flex flex-row space-x-4">
-            <input
-                type="text"
-                placeholder="Prompt"
-                name="prompt"
-                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" />
-            <button
-                type="submit"
-                class="inline-flex items-center rounded-md border border-transparent bg-neutral-800 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2">
-                Generate!
-            </button>
+            <input type="text" placeholder="Prompt" name="prompt" class="prompt-message" />
+            <button type="submit" class="prompt-send"> Generate! </button>
         </span>
 
         <span class="flex flex-row space-x-4">
@@ -59,3 +51,21 @@
         </span>
     </form>
 </main>
+
+<style lang="postcss">
+    .image-form {
+        @apply flex flex-col space-y-2 md:min-w-[28rem] lg:min-w-[32rem] xl:min-w-[36rem] max-w-6xl;
+    }
+
+    .image-output {
+        @apply flex flex-col items-center justify-center w-full aspect-square border-2 border-neutral-300 border-dashed rounded-lg cursor-pointer bg-neutral-50 text-transparent;
+    }
+
+    .prompt-message {
+        @apply block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6;
+    }
+
+    .prompt-send {
+        @apply inline-flex items-center rounded-md border border-transparent bg-neutral-800 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2;
+    }
+</style>
